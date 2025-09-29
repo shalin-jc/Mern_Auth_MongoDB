84c3bd0360afeeb9be00ac1e3ae176bb9148df93
Shalin-Jc <shalin.b@junkiescoder.com>
2025-09-29 13:24:02 +0530

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -8,7 +8,7 @@ export function Welcome(props) {
         
         <div className="text-center  text-white mt-20">
             
-            <h2 className='text-6xl'>Hello {userData ? userData.name: "Developers"}!</h2>
+            <h2 className='text-6xl'>Hello {userData ? userData.name: "Developers"}!!</h2>
             <h1 className='text-3xl'>Welecome to<span className="text-blue-500 cursor-pointer"> Coders.com</span></h1>
             <button className="btn w-100 text-center">Get Started</button>
         </div>
